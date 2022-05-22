419aad79003f2826b70471164e7b27b903f6317c
Xerkes <gianrayogiagone@yahoo.com>
2022-05-22 19:01:03 +0800

    Commit

---
+++ TBG/Assets/New folder/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	public void PlayGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+	
+}
